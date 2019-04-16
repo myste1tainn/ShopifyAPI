6ec7caeb347eb42fe4479c639c5ceebf19444d62
Arnon Keereena <a.keereena@gmail.com>
2019-04-16 22:08:54 +0700

    Add comment for continuation of work

--- Tests/ShopifyAPITests/E2E Tests/APIsTests.swift
+++ Tests/ShopifyAPITests/E2E Tests/APIsTests.swift
@@ -20,6 +20,7 @@ final class APIsTests: XCTestCase {
     XCTAssert(orders != nil && orders?.count ?? 0 > 0, "it returns orders")
   }
 
+  // TODO: still have to tests get all orders scenario where it ended up with unknown errors
 //  func tests_getOrders_whenUsingCorrectShop_itReturnsOrders() {
 //    let orderCount = self.apis.orderCount(shop: self.shop).toBlocking().firstCatchError()
 //    let orders = self.apis.orders(shop: self.shop).toBlocking().firstCatchError()
